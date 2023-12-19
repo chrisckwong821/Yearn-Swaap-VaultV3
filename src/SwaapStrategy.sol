d7aa6cfcde85117b70cc45075891c8e81c209932
personal <chrisckwong821@gmail.com>
2023-12-19 11:41:41 +0800

    remove interfaces, add joinPool using exactOutput

--- src/SwaapStrategy.sol
+++ src/SwaapStrategy.sol
@@ -1,154 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity 0.8.18;
 
-import {BaseStrategy, ERC20} from "lib/tokenized-strategy/src/BaseStrategy.sol";
-interface IERC20Metadata {
-    function decimals() external view returns(uint8);
-}
-
-interface AggregatorV3Interface {
-
-  function latestRoundData()
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-
-}
-interface ISafeguardPool {
-
-    struct InitialOracleParams {
-        AggregatorV3Interface oracle;
-        uint256 maxTimeout;
-        bool isStable;
-        bool isFlexibleOracle;
-    }
-
-    struct OracleParams {
-        AggregatorV3Interface oracle;
-        uint256 maxTimeout;
-        bool isStable;
-        bool isFlexibleOracle;
-        bool isPegged;
-        uint256 priceScalingFactor;
-    }
-    
-    /// @dev returns the current target balances of the pool based on the hodl strategy and latest performance
-    function getHodlBalancesPerPT() external view returns(uint256, uint256);
-    
-    /// @dev returns the on-chain oracle price of tokenIn such that price = amountIn / amountOut
-    function getOnChainAmountInPerOut(address tokenIn) external view returns(uint256);
-    
-    /// @dev returns the current pool oracle parameters
-    function getOracleParams() external view returns(OracleParams[] memory);
-
-}
-
-
-interface ISwaapVault {
-
-    function joinPool(
-        bytes32 poolId,
-        address sender,
-        address recipient,
-        JoinPoolRequest memory request
-    ) external payable;
-
-    struct JoinPoolRequest {
-        ERC20[] assets;
-        uint256[] maxAmountsIn;
-        bytes userData;
-        bool fromInternalBalance;
-    }
-
-   function exitPool(
-        bytes32 poolId,
-        address sender,
-        address payable recipient,
-        ExitPoolRequest memory request
-    ) external;
-
-    struct ExitPoolRequest {
-        ERC20[] assets;
-        uint256[] minAmountsOut;
-        bytes userData;
-        bool toInternalBalance;
-    } 
-
-    function getPoolTokens(bytes32 poolId) external view returns(
-        address[] memory tokens, 
-        uint256[] memory balances,
-        uint256 lastChangeBlock
-    );
-
-    function getPool(bytes32 poolId) external view returns(
-        address poolAddress,
-        uint8 poolSpecialization
-    );
-}
-
-interface IAavePool {
- function deposit(
-    ERC20 asset,
-    uint256 amount,
-    address onBehalfOf,
-    uint16 referralCode
-  ) external;
-
-  function withdraw(
-    ERC20 asset,
-    uint256 amount,
-    address to
-  ) external returns (uint256);
-
-  function borrow(
-    ERC20 asset,
-    uint256 amount,
-    uint256 interestRateMode,
-    uint16 referralCode,
-    address onBehalfOf
-  ) external;
-
-  function repay(
-    ERC20 asset,
-    uint256 amount,
-    uint256 rateMode,
-    address onBehalfOf
-  ) external returns (uint256);
-
-  function getConfiguration(
-    address reserve
-  ) external view returns(uint256);
-
-  function getUserAccountData(address user)
-    external
-    view
-    returns (
-      uint256 totalCollateralETH,
-      uint256 totalDebtETH,
-      uint256 availableBorrowsETH,
-      uint256 currentLiquidationThreshold,
-      uint256 ltv,
-      uint256 healthFactor
-    );
-}
-
-enum Action {
-        deposit,
-        withdraw, 
-        borrow,
-        repay,
-        join, 
-        exit,
-        swap
-}
-
-contract SwaapStrategy is BaseStrategy {
+import {BaseStrategy} from "lib/tokenized-strategy/src/BaseStrategy.sol";
+import {SwaapEncodings} from "./utils/SwaapEncodings.sol";
+import "./utils/Interfaces.sol";
+contract SwaapStrategy is BaseStrategy, SwaapEncodings {
     // dev: the storage of tokenised strat is at completely diff location, hence here proxy aka this contract can have its own storage
     ERC20 internal immutable borrowedAsset;
     ISwaapVault public liquidityPool;
@@ -179,7 +35,6 @@ contract SwaapStrategy is BaseStrategy {
      */
     function _deployFunds(uint256 _amount) internal override {
         /** 
-         * @TODO
          * assume we have 100 excess "want"
          * using "LTV", "price", calculate the amount of "want" to deposit into lending market
          * calculate the amount of "paired" token to borrow
@@ -198,7 +53,25 @@ contract SwaapStrategy is BaseStrategy {
         // _borrowedAssetInUnitOfAsset = priceOfBorrowed / priceAsset
         uint256 borrow = borrowInUnitOfAsset * 1e18 / _borrowedAssetPriceInUnitOfAsset();
         lendingPool.borrow(borrowedAsset, borrow, 2, 0, address(this));
-        // @TODO joinPool using the remaining asset and borrowedAsset
+        // joinPool
+        // the amount of asset to supply into Swaap is the input _amount, minus the amount deposited int Aave
+        uint256 amountA = _amount - deposit;
+        uint256 amountB = borrow;
+        // expect a router to enforce slippage protection on top of the ERC4626 vault level
+        // 0.5%
+        uint256 slippage = 50;
+        uint256 minimumBPT = _calcBPToExpect() * (10000 - slippage) / 100000;
+        ISwaapVault.JoinPoolRequest memory j;
+        j.assets = _gibDynamicArrayERC20(asset, borrowedAsset); // @audit does order matter, refractor this later
+        // @TODO use getUserDataForExactJoin
+        if (asset < borrowedAsset) {
+            j.maxAmountsIn = _gibDynamicArrayUint256(amountA, amountB);
+        } else {
+            j.maxAmountsIn = _gibDynamicArrayUint256(amountB, amountA);
+        }
+        j.userData = getUserDataForProportionalJoin(minimumBPT);
+        liquidityPool.joinPool(poolId, address(this), address(this), j);
+        
     }
 
     /**
@@ -224,13 +97,20 @@ contract SwaapStrategy is BaseStrategy {
      */
     function _freeFunds(uint256 _amount) internal override {
         /** 
-         * @TODO
          * assume we need 100 "want"
          * we need to scale down the entire position (LP, deposit and borrow in lending market)
          * 1.) withdraw some LP
          * 2.) repay the debt 
          * 3.) withdraw some want
          */ 
+        uint256 minimumBPT = 0;
+        uint256 amountA = _amount;
+        // uint256 amountB = borrow;
+        // ISwaapVault.ExitPoolRequest memory e;
+        // e.assets = _gibDynamicArrayERC20(asset, borrowedAsset);
+        // e.minAmountsOut = _gibDynamicArrayUint256(amountA, amountB);
+        // e.userData = getUserDataForProportionalExit(minimumBPT);
+        // liquidityPool.exitPool(poolId, address(this), payable(address(this)), e);
         
     }
     /**
@@ -319,6 +199,7 @@ contract SwaapStrategy is BaseStrategy {
     // hacky way to get around, refractor if time permits
     function _gibDynamicArrayERC20(ERC20 a, ERC20 b) internal returns (ERC20[] memory)
     {
+           (a, b) = _sortTwoTokens(a, b);
            ERC20[] memory dynamicArray = new ERC20[](2);
            dynamicArray[0] = a;
            dynamicArray[1] = b;
@@ -331,6 +212,13 @@ contract SwaapStrategy is BaseStrategy {
            dynamicArray[0] = a;
            dynamicArray[1] = b;
            return dynamicArray;
+    }
+
+    /**
+     * @dev Sorts two tokens in ascending order, returning them as a (tokenA, tokenB) tuple.
+     */
+    function _sortTwoTokens(ERC20 tokenX, ERC20 tokenY) private pure returns (ERC20, ERC20) {
+        return tokenX < tokenY ? (tokenX, tokenY) : (tokenY, tokenX);
     }
 
     /**
@@ -348,7 +236,7 @@ contract SwaapStrategy is BaseStrategy {
      * X is 62.5, the borrowed value is (100 - 62.5) * 40/60 => 25
     
 
-     * generallize : X * liqThre * poolRatio = targetHF * (worth - X)
+     * generallize : X * liqThre * poolRatio(asset/borrowedAsset) = targetHF * (worth - X)
 
                                       targetHF * worth
             X =           --------------------------------------- 
@@ -388,12 +276,14 @@ contract SwaapStrategy is BaseStrategy {
     function _retrieveAaveLiquidationThreshold() internal view returns(uint256 liquidationThreshold) {
         uint256 configuration = lendingPool.getConfiguration(address(asset));
         // copied from Aave, reference DataTypes;
-        // https://github.com/aave/aave-v3-core/blob/master/contracts/protocol/libraries/configuration/ReserveConfiguration.sol#L109-L113
+        // https://github.com/aave/protocol-v2/blob/master/contracts/protocol/libraries/configuration/ReserveConfiguration.sol#L80-L86
         uint256 LIQUIDATION_THRESHOLD_MASK = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF0000FFFF;
         uint256 LIQUIDATION_THRESHOLD_START_BIT_POSITION = 16;
         liquidationThreshold = 
             (configuration & ~LIQUIDATION_THRESHOLD_MASK) >> LIQUIDATION_THRESHOLD_START_BIT_POSITION;
     }
+
+
 
     function getLPValue() public view returns(uint256 lpValue) {
         (address poolAddress, ) = liquidityPool.getPool(poolId);
@@ -406,8 +296,15 @@ contract SwaapStrategy is BaseStrategy {
         oracleParameter = ISafeguardPool(poolAddress).getOracleParams();
         (,int256 token0Price,,,) = oracleParameter[0].oracle.latestRoundData();
         (,int256 token1Price,,,) = oracleParameter[1].oracle.latestRoundData();
-        uint256 token0Value = balances[0] * uint256(token0Price);
-        uint256 token1Value = balances[1] * uint256(token1Price);
+        uint256 token0Value;
+        uint256 token1Value;
+        if (asset < borrowedAsset) {
+            token0Value = tokenAmountIn18Decimals(asset, balances[0]) * uint256(token0Price);
+            token1Value = tokenAmountIn18Decimals(borrowedAsset, balances[1]) * uint256(token1Price);
+        } else {
+            token0Value = tokenAmountIn18Decimals(borrowedAsset, balances[0]) * uint256(token0Price);
+            token1Value = tokenAmountIn18Decimals(asset, balances[1]) * uint256(token1Price);
+        }
         // assume price is 1e8
         return currentBalance * (token0Value + token1Value) / totalSupply / 1e8;
     }
@@ -434,8 +331,38 @@ contract SwaapStrategy is BaseStrategy {
             return uint256(token1Price * 1e18 / token0Price);
         }
     }
-    function tokenAmountIn18Decimals(address asset, uint256 amount) public view returns(uint256) {
-        return amount * 1e18 / (10 ** IERC20Metadata(asset).decimals());
+
+    function _calcBPToExpect() internal view returns(uint256) {
+        (address poolAddress, ) = liquidityPool.getPool(poolId);
+        uint256[] memory balances = new uint256[](2);
+        (,balances,) = ISwaapVault(liquidityPool).getPoolTokens(poolId);
+        uint256 totalSupply = ERC20(poolAddress).totalSupply();
+        // fetch price from the oracle
+        ISafeguardPool.OracleParams[] memory oracleParameter = new ISafeguardPool.OracleParams[](2);
+        oracleParameter = ISafeguardPool(poolAddress).getOracleParams();
+        (,int256 token0Price,,,) = oracleParameter[0].oracle.latestRoundData();
+        (,int256 token1Price,,,) = oracleParameter[1].oracle.latestRoundData();
+        uint256 token0Value;
+        uint256 token1Value;
+        uint256 currentToken0Value;
+        uint256 currentToken1Value;
+        if (asset < borrowedAsset) {
+            token0Value = tokenAmountIn18Decimals(asset, balances[0]) * uint256(token0Price);
+            token1Value = tokenAmountIn18Decimals(borrowedAsset, balances[1]) * uint256(token1Price);
+            currentToken0Value = tokenAmountIn18Decimals(asset, ERC20(asset).balanceOf(address(this))) * uint256(token0Price);
+            currentToken1Value = tokenAmountIn18Decimals(borrowedAsset, ERC20(borrowedAsset).balanceOf(address(this))) * uint256(token1Price);
+        } else {
+            token0Value = tokenAmountIn18Decimals(borrowedAsset, balances[0]) * uint256(token0Price);
+            token1Value = tokenAmountIn18Decimals(asset, balances[1]) * uint256(token1Price);
+            currentToken0Value = tokenAmountIn18Decimals(borrowedAsset, ERC20(borrowedAsset).balanceOf(address(this))) * uint256(token0Price);
+            currentToken1Value = tokenAmountIn18Decimals(asset, ERC20(asset).balanceOf(address(this))) * uint256(token1Price);
+        }
+        // assume price is 1e8
+        return (currentToken0Value + currentToken1Value) * totalSupply / (token0Value + token1Value);
     }
+    function tokenAmountIn18Decimals(ERC20 asset, uint256 amount) public view returns(uint256) {
+        return amount * 1e18 / (10 ** IERC20Metadata(address(asset)).decimals());
+    }
+
 }
 
